0b811079399cb600500bbf42bbca867491b9d778
agerencher <agerencher1@hwemail.com>
2024-09-20 09:54:43 -0700

    Milestone #1 Finished - No Tester Creates an Index file in a objects directory in a git directory

--- Git.java
+++ Git.java
@@ -1,5 +1,17 @@
+import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 public class Git{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File git = new File("git");
+        File objects = new File(git, "objects");
+        File index = new File(objects, "index");
+        if (git.exists() && objects.exists() && index.exists())
+            System.out.println ("Git Repository already exists");
+        else
+            git.mkdir(); objects.mkdirs(); index.createNewFile();
         
+    
     }
 }
